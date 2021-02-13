b588c047445115be3acaf39014ff56ce6dc5f821
rratilal <rachidratilal@gmail.com>
2021-02-13 13:49:53 +0200

    setuping connection to database

--- dist/database/connections.js
+++ dist/database/connections.js
@@ -2,3 +2,29 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const typeorm_1 = require("typeorm");
 typeorm_1.createConnection();
+const getOptions = async () => {
+    let connectionOptions;
+    connectionOptions = {
+        type: "postgres",
+        logging: false,
+        extra: {
+            ssl: true,
+        },
+        synchronize: false,
+        entities: [
+            "dist/models/*.js"
+        ]
+    };
+    if (process.env.DATABASE_URL) {
+        Object.assign(connectionOptions, { url: process.env.DATABASE_URL });
+    }
+    else {
+        connectionOptions = await typeorm_1.getConnectionOptions();
+    }
+    return connectionOptions;
+};
+const connect2Database = async () => {
+    const typeormconfig = await getOptions();
+    await typeorm_1.createConnection(typeormconfig);
+};
+exports.default = connect2Database;
--- src/database/connections.ts
+++ src/database/connections.ts
@@ -1,3 +1,31 @@
-import { createConnection } from 'typeorm';
+import { ConnectionOptions, createConnection, getConnectionOptions } from 'typeorm';
 
-createConnection();
+createConnection()
+
+const getOptions = async () => {
+    let connectionOptions: ConnectionOptions;
+    connectionOptions = {
+        type: "postgres",
+        logging: false,
+        extra: {
+        ssl: true,
+        },
+        synchronize: false,
+        entities: [
+            "dist/models/*.js"
+        ]
+    };
+    if (process.env.DATABASE_URL) {
+        Object.assign(connectionOptions, { url: process.env.DATABASE_URL })
+    } else {
+        connectionOptions = await getConnectionOptions(); 
+    }
+    return connectionOptions;
+}
+
+const connect2Database = async (): Promise<void> => {
+    const typeormconfig = await getOptions();
+    await createConnection(typeormconfig);
+};
+
+export default connect2Database
